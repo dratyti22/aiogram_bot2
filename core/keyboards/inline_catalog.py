7750c3dc33ff8da3666bd4685d33bb911d26a5d9
dratyti22 <137050298+dratyti22@users.noreply.github.com>
2023-12-25 12:48:37 +0300

    добавленна клавиатура для таблиц из бд

--- core/keyboards/inline_catalog.py
+++ core/keyboards/inline_catalog.py
@@ -1,10 +1,86 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-from core.database.db_products_create import display_brawl_stars_db
+from core.database.db_products_create import display_brawl_stars_db, display_clash_of_clans_db, \
+    display_pubg_mobaile_db, display_clash_royale_db, display_codm_db
 
 
 def inline_catalog_brawl_stars():
     entries = display_brawl_stars_db()
+
+    inline_buttons = [
+        [
+            InlineKeyboardButton(
+                text=entry[1],
+                callback_data=f'product_{entry[0]}_brawl_stars'
+            ),
+        ] for entry in entries
+    ]
+    inline_buttons.append([
+        InlineKeyboardButton(text='Назад', callback_data='back_catalog')
+    ])
+
+    kb = InlineKeyboardMarkup(inline_keyboard=inline_buttons)
+    return kb
+
+
+def inline_catalog_clash_of_clans():
+    entries = display_clash_of_clans_db()
+
+    inline_buttons = [
+        [
+            InlineKeyboardButton(
+                text=entry[1],
+                callback_data=f'product_{entry[0]}_brawl_stars'
+            ),
+        ] for entry in entries
+    ]
+    inline_buttons.append([
+        InlineKeyboardButton(text='Назад', callback_data='back_catalog')
+    ])
+
+    kb = InlineKeyboardMarkup(inline_keyboard=inline_buttons)
+    return kb
+
+
+def inline_catalog_pubg_mobaile():
+    entries = display_pubg_mobaile_db()
+
+    inline_buttons = [
+        [
+            InlineKeyboardButton(
+                text=entry[1],
+                callback_data=f'product_{entry[0]}_brawl_stars'
+            ),
+        ] for entry in entries
+    ]
+    inline_buttons.append([
+        InlineKeyboardButton(text='Назад', callback_data='back_catalog')
+    ])
+
+    kb = InlineKeyboardMarkup(inline_keyboard=inline_buttons)
+    return kb
+
+
+def inline_catalog_clash_royale():
+    entries = display_clash_royale_db()
+
+    inline_buttons = [
+        [
+            InlineKeyboardButton(
+                text=entry[1],
+                callback_data=f'product_{entry[0]}_brawl_stars'
+            ),
+        ] for entry in entries
+    ]
+    inline_buttons.append([
+        InlineKeyboardButton(text='Назад', callback_data='back_catalog')
+    ])
+
+    kb = InlineKeyboardMarkup(inline_keyboard=inline_buttons)
+    return kb
+
+
+def inline_catalog_codm():
+    entries = display_codm_db()
 
     inline_buttons = [
         [
