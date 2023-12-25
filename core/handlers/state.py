6f0fb7f7566146ac51824e63bd65203bec4abe01
dratyti22 <137050298+dratyti22@users.noreply.github.com>
2023-12-25 12:54:15 +0300

    добавленна состояние для других продуктов

--- core/handlers/state.py
+++ core/handlers/state.py
@@ -1,7 +1,31 @@
 from aiogram.fsm.state import StatesGroup, State
 
 
-class AddProductAdminState(StatesGroup):
+class AddProductAdminBsState(StatesGroup):
+    MAIN = State()
+    NAME = State()
+    PRICE = State()
+
+
+class AddProductAdminCocState(StatesGroup):
+    MAIN = State()
+    NAME = State()
+    PRICE = State()
+
+
+class AddProductAdminPmState(StatesGroup):
+    MAIN = State()
+    NAME = State()
+    PRICE = State()
+
+
+class AddProductAdminCrState(StatesGroup):
+    MAIN = State()
+    NAME = State()
+    PRICE = State()
+
+
+class AddProductAdminCodmState(StatesGroup):
     MAIN = State()
     NAME = State()
     PRICE = State()
