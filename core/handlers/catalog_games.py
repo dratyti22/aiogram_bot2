f1aa7945bf37a92f9e1438e66e7e167721f655c4
dratyti22 <137050298+dratyti22@users.noreply.github.com>
2023-12-25 14:38:18 +0300

    добавленно: inline клавиатура для товаров

--- core/handlers/catalog_games.py
+++ core/handlers/catalog_games.py
@@ -1,7 +1,8 @@
 from aiogram import Bot, F, Router
 from aiogram.types import CallbackQuery
 
-from core.keyboards.inline_catalog import inline_catalog_brawl_stars
+from core.keyboards.inline_catalog import inline_catalog_brawl_stars, inline_catalog_codm, \
+    inline_catalog_clash_royale, inline_catalog_pubg_mobaile, inline_catalog_clash_of_clans
 from core.keyboards.inline import catalog_inline
 
 router = Router()
@@ -9,8 +10,42 @@ router = Router()
 
 @router.callback_query(F.data == 'brawl_stars')
 async def display_brawl_stars_catalog(callback: CallbackQuery, bot: Bot):
+    entries = inline_catalog_brawl_stars()
+
     await bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.message_id,
-                                text='Выбери товар', reply_markup=inline_catalog_brawl_stars())
+                                text='Выбери товар', reply_markup=entries)
+
+
+@router.callback_query(F.data == 'codm')
+async def display_brawl_stars_catalog(callback: CallbackQuery, bot: Bot):
+    entries = inline_catalog_codm()
+
+    await bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.message_id,
+                                text='Выбери товар', reply_markup=entries)
+
+
+@router.callback_query(F.data == 'clash_royale')
+async def display_brawl_stars_catalog(callback: CallbackQuery, bot: Bot):
+    entries = inline_catalog_clash_royale()
+
+    await bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.message_id,
+                                text='Выбери товар', reply_markup=entries)
+
+
+@router.callback_query(F.data == 'pubg_mobaile')
+async def display_brawl_stars_catalog(callback: CallbackQuery, bot: Bot):
+    entries = inline_catalog_pubg_mobaile()
+
+    await bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.message_id,
+                                text='Выбери товар', reply_markup=entries)
+
+
+@router.callback_query(F.data == 'clash_of_clans')
+async def display_brawl_stars_catalog(callback: CallbackQuery, bot: Bot):
+    entries = inline_catalog_clash_of_clans()
+
+    await bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.message_id,
+                                text='Выбери товар', reply_markup=entries)
 
 
 @router.callback_query(F.data == 'back_catalog')
--- core/keyboards/inline_catalog.py
+++ core/keyboards/inline_catalog.py
@@ -5,94 +5,117 @@ from core.database.db_products_create import display_brawl_stars_db, display_cla
 
 def inline_catalog_brawl_stars():
     entries = display_brawl_stars_db()
+    if len(entries) > 0:
+        inline_buttons = [
+            [
+                InlineKeyboardButton(
+                    text=entry[1],
+                    callback_data=f'product_{entry[0]}_brawl_stars'
+                ),
+            ] for entry in entries
+        ]
+        inline_buttons.append([
+            InlineKeyboardButton(text='Назад', callback_data='back_catalog')
+        ])
 
-    inline_buttons = [
-        [
-            InlineKeyboardButton(
-                text=entry[1],
-                callback_data=f'product_{entry[0]}_brawl_stars'
-            ),
-        ] for entry in entries
-    ]
-    inline_buttons.append([
-        InlineKeyboardButton(text='Назад', callback_data='back_catalog')
-    ])
-
-    kb = InlineKeyboardMarkup(inline_keyboard=inline_buttons)
-    return kb
+        kb = InlineKeyboardMarkup(inline_keyboard=inline_buttons)
+        return kb
+    else:
+        return inline_catalog_back()
 
 
 def inline_catalog_clash_of_clans():
     entries = display_clash_of_clans_db()
 
-    inline_buttons = [
-        [
-            InlineKeyboardButton(
-                text=entry[1],
-                callback_data=f'product_{entry[0]}_brawl_stars'
-            ),
-        ] for entry in entries
-    ]
-    inline_buttons.append([
-        InlineKeyboardButton(text='Назад', callback_data='back_catalog')
-    ])
+    if len(entries) > 0:
+        inline_buttons = [
+            [
+                InlineKeyboardButton(
+                    text=entry[1],
+                    callback_data=f'product_{entry[0]}_brawl_stars'
+                ),
+            ] for entry in entries
+        ]
+        inline_buttons.append([
+            InlineKeyboardButton(text='Назад', callback_data='back_catalog')
+        ])
 
-    kb = InlineKeyboardMarkup(inline_keyboard=inline_buttons)
-    return kb
+        kb = InlineKeyboardMarkup(inline_keyboard=inline_buttons)
+        return kb
+    else:
+        return inline_catalog_back()
 
 
 def inline_catalog_pubg_mobaile():
     entries = display_pubg_mobaile_db()
 
-    inline_buttons = [
-        [
-            InlineKeyboardButton(
-                text=entry[1],
-                callback_data=f'product_{entry[0]}_brawl_stars'
-            ),
-        ] for entry in entries
-    ]
-    inline_buttons.append([
-        InlineKeyboardButton(text='Назад', callback_data='back_catalog')
-    ])
+    if len(entries) > 0:
+        inline_buttons = [
+            [
+                InlineKeyboardButton(
+                    text=entry[1],
+                    callback_data=f'product_{entry[0]}_brawl_stars'
+                ),
+            ] for entry in entries
+        ]
+        inline_buttons.append([
+            InlineKeyboardButton(text='Назад', callback_data='back_catalog')
+        ])
 
-    kb = InlineKeyboardMarkup(inline_keyboard=inline_buttons)
-    return kb
+        kb = InlineKeyboardMarkup(inline_keyboard=inline_buttons)
+        return kb
+    else:
+        return inline_catalog_back()
 
 
 def inline_catalog_clash_royale():
     entries = display_clash_royale_db()
 
-    inline_buttons = [
-        [
-            InlineKeyboardButton(
-                text=entry[1],
-                callback_data=f'product_{entry[0]}_brawl_stars'
-            ),
-        ] for entry in entries
-    ]
-    inline_buttons.append([
-        InlineKeyboardButton(text='Назад', callback_data='back_catalog')
-    ])
+    if len(entries) > 0:
+        inline_buttons = [
+            [
+                InlineKeyboardButton(
+                    text=entry[1],
+                    callback_data=f'product_{entry[0]}_brawl_stars'
+                ),
+            ] for entry in entries
+        ]
+        inline_buttons.append([
+            InlineKeyboardButton(text='Назад', callback_data='back_catalog')
+        ])
 
-    kb = InlineKeyboardMarkup(inline_keyboard=inline_buttons)
-    return kb
+        kb = InlineKeyboardMarkup(inline_keyboard=inline_buttons)
+        return kb
+    else:
+        return inline_catalog_back()
 
 
 def inline_catalog_codm():
     entries = display_codm_db()
 
-    inline_buttons = [
-        [
-            InlineKeyboardButton(
-                text=entry[1],
-                callback_data=f'product_{entry[0]}_brawl_stars'
-            ),
-        ] for entry in entries
-    ]
-    inline_buttons.append([
-        InlineKeyboardButton(text='Назад', callback_data='back_catalog')
-    ])
+    if len(entries) > 0:
+        inline_buttons = [
+            [
+                InlineKeyboardButton(
+                    text=entry[1],
+                    callback_data=f'product_{entry[0]}_brawl_stars'
+                ),
+            ] for entry in entries
+        ]
+        inline_buttons.append([
+            InlineKeyboardButton(text='Назад', callback_data='back_catalog')
+        ])
 
-    kb = InlineKeyboardMarkup(inline_keyboard=inline_buttons)
+        kb = InlineKeyboardMarkup(inline_keyboard=inline_buttons)
+        return kb
+    else:
+        return inline_catalog_back()
+
+
+def inline_catalog_back():
+    kb = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text='Назад', callback_data='back_catalog')
+        ]
+    ])
     return kb
