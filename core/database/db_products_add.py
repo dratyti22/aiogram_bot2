461c7164c99cde4ffe7303a60edf9050fddd8312
dratyti22 <137050298+dratyti22@users.noreply.github.com>
2023-12-25 12:43:49 +0300

    Добавленно добаление товара в другие таблицы

--- core/database/db_products_add.py
+++ core/database/db_products_add.py
@@ -7,3 +7,23 @@ cur = conn.cursor()
 async def add_product_brawl_stars_db(name: str, price: int):
     cur.execute('''INSERT INTO brawl_stars (name, price) VALUES (?, ?)''', (name, price,))
     conn.commit()
+
+
+async def add_product_clash_royale_db(name: str, price: int):
+    cur.execute('''INSERT INTO clash_royale (name, price) VALUES (?, ?)''', (name, price,))
+    conn.commit()
+
+
+async def add_product_clash_of_clans_db(name: str, price: int):
+    cur.execute('''INSERT INTO clash_of_clans (name, price) VALUES (?, ?)''', (name, price,))
+    conn.commit()
+
+
+async def add_product_pubg_mobaile_db(name: str, price: int):
+    cur.execute('''INSERT INTO pubg_mobaile (name, price) VALUES (?, ?)''', (name, price,))
+    conn.commit()
+
+
+async def add_product_codm_db(name: str, price: int):
+    cur.execute('''INSERT INTO codm (name, price) VALUES (?, ?)''', (name, price,))
+    conn.commit()
