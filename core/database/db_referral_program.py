195789c2b84aa6de4879acabe9a678d7c4b4b8b6
dratyti22 <137050298+dratyti22@users.noreply.github.com>
2023-12-27 17:56:23 +0300

    Создана бд для хранения реф. прог., и показа данных

---
+++ core/database/db_referral_program.py
@@ -0,0 +1,30 @@
+import sqlite3 as sq
+
+conn = sq.connect('aiogram2.db')
+cur = conn.cursor()
+
+
+async def create_referral_program_db(tg_id: int, link: str):
+    cur.execute(
+        '''CREATE TABLE IF NOT EXISTS referral_program (
+        user_id INTEGER PRIMARY KEY,
+        link TEXT,
+        price INTEGER,
+        who_clicked_on_the_ink INTEGER
+        )'''
+    )
+    conn.commit()
+
+    cur.execute(
+        '''INSERT OR IGNORE INTO referral_program (user_id, link, price, who_clicked_on_the_ink) 
+        VALUES (?, ?, ?, ?)''',
+        (tg_id, link, 0, 0)
+    )
+    conn.commit()
+
+
+async def display_referral_program_db():
+    cur.execute('''SELECT * FROM referral_program''')
+    entry = cur.fetchall()
+    conn.commit()
+    return entry
