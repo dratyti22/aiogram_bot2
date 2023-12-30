ea4ff2a11e0b1fd8ad8c1ac223915a8c08379f2a
dratyti22 <137050298+dratyti22@users.noreply.github.com>
2023-12-30 17:16:50 +0300

    зарегистрирован pay_in_catalog

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import asyncio
 import logging
 
-from core.handlers import starting, catalog_games, admin_processing, referral_program
+from core.handlers import starting, catalog_games, admin_processing, referral_program,  pay_in_catalog
 
 
 async def starting_bot(bot: Bot):
@@ -24,7 +24,8 @@ async def main():
     dp.startup.register(starting_bot)
     dp.shutdown.register(stop_bot)
 
-    dp.include_routers(starting.router, catalog_games.router, admin_processing.router, referral_program.router)
+    dp.include_routers(starting.router, catalog_games.router, admin_processing.router, referral_program.router,
+                       pay_in_catalog.router)
 
     try:
         await bot.delete_webhook(drop_pending_updates=True)
