bc6f46474b3bd83ce8e4e2970b51ae1f88d13b09
dratyti22 <137050298+dratyti22@users.noreply.github.com>
2024-01-05 18:11:16 +0300

    Рассылка сообщения пользователям

--- core/handlers/admin_processing.py
+++ core/handlers/admin_processing.py
@@ -1,4 +1,4 @@
-from aiogram import F, Router
+from aiogram import F, Router, Bot
 from aiogram.types import CallbackQuery, Message
 from aiogram.fsm.context import FSMContext
 import os
@@ -6,11 +6,12 @@ import os
 from core.keyboards.inline import catalog_inline_admin
 from core.handlers.state import AddProductAdminState, AddProductAdminBsState, AddProductAdminCodmState, \
     AddProductAdminCocState, AddProductAdminCrState, AddProductAdminPmState, DeletedProductAdminState, \
-    DisplayProductInCatalogState
+    DisplayProductInCatalogState, TextMailingListState
 from core.database.db_products_add import add_product_brawl_stars_db, add_product_codm_db, \
     add_product_clash_of_clans_db, add_product_clash_royale_db, add_product_pubg_mobaile_db
 from core.database.db_product_deleted import deleted_products_db
 from core.database.db_products_create import display_db
+from core.database.db_user_id import set_activate_db, get_users_db
 
 router = Router()
 
@@ -21,7 +22,7 @@ async def display_products_in_category(message: Message, state: FSMContext):
         await message.answer(text='Выбери категорию игр:', reply_markup=catalog_inline_admin())
         await state.set_state(DisplayProductInCatalogState.MAIN)
     else:
-        await message.answer(text='Ты не админn\nЗабудь эту команду!')
+        await message.answer(text='Ты не админ\nЗабудь эту команду!')
 
 
 @router.callback_query(DisplayProductInCatalogState.MAIN)
@@ -191,4 +192,29 @@ async def add_product_brawl_stars_price(message: Message, state: FSMContext):
     data = await state.get_data()
     await add_product_pubg_mobaile_db(str(data['name']), int(data['price']))
     await message.answer(text='Товар добавлен')
+    await state.clear()
+
+
+@router.message(F.text == 'Сделать рассылку')
+async def make_a_newsletter(message: Message, state: FSMContext):
+    if message.from_user.id == int(os.getenv('ADMIN_ID')):
+        await message.answer("Напиши сообщение и отправь его мне:")
+        await state.set_state(TextMailingListState.TEXT)
+    else:
+        await message.answer('Вы не являетесь админом')
+
+
+@router.message(TextMailingListState.TEXT)
+async def make_a_newsletter_text(message: Message, state: FSMContext, bot: Bot):
+    await state.update_data(text=message.text)
+    data = await state.get_data()
+    users = get_users_db()
+    for i in users:
+        try:
+            await bot.send_message(i[0], data['text'])
+            if int(i[1]) != 1:
+                set_activate_db(i[0], 1)
+        except:
+            set_activate_db(i[0], 0)
+    await bot.send_message(message.from_user.id, 'Рассылка завершена')
     await state.clear()
