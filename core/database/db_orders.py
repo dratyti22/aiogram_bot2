df612b9c67bb282ac986c33bd0935094f3abc2f1
dratyti22 <137050298+dratyti22@users.noreply.github.com>
2024-01-05 17:20:31 +0300

    Исправленна ошибка с NoneType

--- core/database/db_orders.py
+++ core/database/db_orders.py
@@ -4,7 +4,7 @@ conn = sq.connect('aiogram2.db')
 cur = conn.cursor()
 
 
-async def save_orders_db(name_products: str, price_products: int, time_products: str, id_products: str,
+async def save_orders_db(name_products: str, price_products: int, time_products: str, id_product: str,
                          email_products: str):
     cur.execute('''CREATE TABLE IF NOT EXISTS orders(
         id_products INTEGER PRIMARY KEY,
@@ -16,7 +16,7 @@ async def save_orders_db(name_products: str, price_products: int, time_products:
     )''')
     conn.commit()
     cur.execute('''INSERT INTO orders(name, price, time, id, email) VALUES (?, ?, ?, ?, ?)''',
-                (name_products, price_products, time_products, id_products, email_products))
+                (name_products, price_products, time_products, id_product, email_products))
     conn.commit()
 
 
@@ -27,9 +27,7 @@ def display_orders_db():
     return entry
 
 
-def display_orders_id_db(product_id):
-    db_id = product_id.rstrip('orders_')
-    cur.execute("SELECT name, price, time, id FROM orders WHERE id_products = ?", (db_id,))
-    result = cur.fetchone()
-    if result:
-        return result
+async def display_orders_id_db(product_id):
+    cur.execute("SELECT name, price, time, id FROM orders WHERE id_products =?", (product_id,))
+    result = cur.fetchall()
+    return result
--- core/handlers/orders.py
+++ core/handlers/orders.py
@@ -3,7 +3,7 @@ from aiogram.types import CallbackQuery
 
 from core.keyboards.inline import orders_inline
 from core.database.db_orders import display_orders_id_db
-from core.keyboards.inline_catalog import inline_catalog_back
+from core.keyboards.inline import menu_back_inline, start_inline
 
 router = Router()
 
@@ -14,9 +14,24 @@ async def get_my_orders(call: CallbackQuery, bot: Bot):
                                 reply_markup=orders_inline())
 
 
-@router.callback_query(F.data.startswith('orders_'))
+@router.callback_query(F.data.startswith('order_'))
 async def get_orders_id(callback: CallbackQuery):
-    entries = display_orders_id_db(callback.data)
-    text = ''.join(f'Name: {entry[0]}\nPrice: {entry[1]}\nTime: {entry[2]}\nId: {entry[3]}' for entry in entries)
-    await callback.bot.edit_message_text(message_id=callback.message.message_id, chat_id=callback.message.chat.id,
-                                         text=text, reply_markup=inline_catalog_back())
+    entries = await display_orders_id_db(callback.data.split('_')[1])
+
+    text = ''.join(
+        f'Name: {entry[0]}\nPrice: {entry[1]}\nTime: {entry[2]}\nId: {entry[3]}'
+        for entry in entries
+    )
+    await callback.bot.edit_message_text(
+        message_id=callback.message.message_id,
+        chat_id=callback.message.chat.id,
+        text=text,
+        reply_markup=menu_back_inline()
+    )
+
+
+@router.callback_query(F.data == 'menu_back')
+async def get_menu_back(callback: CallbackQuery):
+    await callback.bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.message_id,
+                                         text='Добро пожаловать в магазин игр! Здесь вы найдете широкий ассортимент игр для всех желаний и интересов.',
+                                         reply_markup=start_inline())
